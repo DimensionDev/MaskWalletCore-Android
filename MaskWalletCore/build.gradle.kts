9c6839ed336ce7fcc673543dde91f9890d43b1b1
Tlaster <tlaster@outlook.com>
2021-05-13 17:11:51 +0800

    add MaskWalletCore reference

--- MaskWalletCore/build.gradle.kts
+++ MaskWalletCore/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.google.protobuf.gradle.*
+
 plugins {
     id("com.android.library")
     id("kotlin-android")
@@ -19,16 +21,39 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    sourceSets {
+        getByName("main") {
+            proto {
+                srcDir("src/main/rust/MaskWalletCore/chain-common/proto")
+            }
+        }
+    }
 }
 
 dependencies {
+    implementation("com.google.protobuf:protobuf-javalite:3.16.0")
     testImplementation("junit:junit:4.+")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
 }
 
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.16.0"
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.plugins {
+                create("java") {
+                    option("lite")
+                }
+            }
+        }
+    }
+}
+
 cargo {
-    module = "./src/main/rust"
+    module = "./src/main/rust/wrapper"
     targets = listOf("arm", "x86", "arm64", "x86_64")
     libname = "maskwalletdroid"
     profile = "release"
@@ -40,5 +65,9 @@ task<Exec>("cargoClean") {
     workingDir("$projectDir/${cargo.module}")
 }
 
+task("protoClean") {
+    delete(protobuf.protobuf.generatedFilesBaseDir)
+}
+
 tasks.getByName("preBuild").dependsOn("cargoBuild")
-tasks.getByName("clean").dependsOn("cargoClean")
+tasks.getByName("clean").dependsOn("cargoClean", "protoClean")
