bf153c5896ced5a1aeadfd0c7b579760fb31f040
Tlaster <tlaster@outlook.com>
2021-05-20 14:40:54 +0800

    clean up gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,21 +4,15 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.2.0")
+        classpath("com.android.tools.build:gradle:4.2.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.0")
     }
 }
 
-plugins {
-    id("org.gradle.android.cache-fix") version "2.3.1" apply false
-}
 subprojects {
     repositories {
         google()
         mavenCentral()
-    }
-    plugins.withType<com.android.build.gradle.BasePlugin>() {
-        apply(plugin = "org.gradle.android.cache-fix")
     }
 }
 
