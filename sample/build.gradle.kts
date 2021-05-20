5ffc45fb8c95db0bfb9e43831116585458f17cba
Tlaster <tlaster@outlook.com>
2021-05-20 17:48:37 +0800

    add reference to MaskWalletCore

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -33,6 +33,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":MaskWalletCore"))
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("com.google.android.material:material:1.2.1")
